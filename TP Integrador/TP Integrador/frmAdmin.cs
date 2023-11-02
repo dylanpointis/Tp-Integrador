d443f3933daef4e293a63b6fd1b62a6eaa970626
Joaco423 <ajoaquinbustamante@hotmail.com>
2023-11-02 12:29:58 -0300

    Grafico

--- TP Integrador/TP Integrador/frmAdmin.cs
+++ TP Integrador/TP Integrador/frmAdmin.cs
@@ -19,7 +19,15 @@ namespace TP_Integrador
 
         private void frmAdmin_Load(object sender, EventArgs e)
         {
+            cargarGrafico();
+        }
 
+        private void cargarGrafico()
+        {
+            double[] yValores = { 12, 23, 10.2, 18.7, 8.5, 1 , 7 , 8 ,9 , 10 , 11 ,12   };
+            string[] xValores = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+            chart1.Series[0].Points.DataBindXY(xValores, yValores);
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
         }
     }
 }
